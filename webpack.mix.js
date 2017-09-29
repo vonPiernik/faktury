df23f142afae2fad02885b47eb7a56bbceaa425b
vonPiernik <norbini12@gmail.com>
2017-09-29 23:12:14 +0200

    Modify webpack setting to not load JS with Vue, Make bootstrap container wider

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
+/*
+Default settings, with loading js and Vue
+
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+*/
+
+mix.sass('resources/assets/sass/app.scss', 'public/css');
