c75d129462fc6c1df7824b673b23d4b585f6fb47
vonPiernik <norbini12@gmail.com>
2017-09-29 23:32:06 +0200

    Modyfikuj wygląd listy faktur (wyświetlaj w tabeli zamiast w liście)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Twoje faktury</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -13,15 +13,16 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    Jesteś zalogowany
-
-                    <hr>
-                    Lista faktur:
-                    @foreach($invoices as $invoice)
-                        <li>{{ $invoice->title }} (<strong>nr {{ $invoice->id }}</strong>) <small>z dnia {{ $invoice->created_at->format('d M Y') }}</small></li>
-                    @endforeach
-    
+                    <table class="table table-striped">
+                        <thead>
+                            <tr><td>Numer</td><td>Tytuł faktury</td><td>Data wystawienia</td></tr>
+                        </thead>
+                        <tbody>
+                        @foreach($invoices as $invoice)
+                            <tr><td>{{ $invoice->id }}</td><td>{{ $invoice->title }}</td><td> {{ $invoice->created_at->format('d M Y') }}</td></tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                     <hr>
 
                     <strong>Branch faktury</strong>
