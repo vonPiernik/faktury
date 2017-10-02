9dbc478a768cb607cb171e3dc2c81fb694a77b5f
vonPiernik <norbini12@gmail.com>
2017-10-02 22:38:59 +0200

    Dropne poprawki w szablonie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,26 +69,21 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/faktury') }}">Panel użytkownika</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Zaloguj się</a>
+                        <a href="{{ route('register') }}">Zarejestruj</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fakturnis
                 </div>
+                    <h2>Prosty i lekki system fakturowy dla małych firm</h2>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                
             </div>
         </div>
     </body>
